6347ca3486de67edcc0b0a6ffa28c91317337331
LorenzoCiarpaglini <98763178+LorenzoCiarpaglini@users.noreply.github.com>
2022-11-16 12:03:28 +0100

    esercizi easy data structure

---
+++ Esercitazioni/16-11/JS/data-structure/easy/esercizio1/esercizio.js
@@ -0,0 +1,8 @@
+/*
+1)Ricerca elemento in array
+
+2)Uguaglianza di array
+
+3)Somma di un array
+
+*/
---
+++ Esercitazioni/16-11/JS/data-structure/easy/esercizio3/esercizio.js
@@ -0,0 +1,33 @@
+/*
+1)isValidSubsequence: Dati in input due array, arr1 e arr2, return true se arr2 
+è un sub array di arr 1, else false.
+
+Es.
+ar1=[1,2,3,4,5]
+ar2=[1,2,3,4,5]
+True
+
+ar1=[1,2,3,4,5]
+ar2=[1,2,3,4]
+True
+
+ar1=[1,2,3,4,5]
+ar2=[2,3]
+True
+
+ar1=[1,2,3,4,5]
+ar2=[3]
+True
+
+ar1=[1,2,3,4,5]
+ar2=[1,2,3,4,6]
+False
+
+ar1=[1,2,3,4,5]
+ar2=[6]
+False
+
+ar1=[1,2,3,4,5]
+ar2=[]
+True
+*/
