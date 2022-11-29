f1c135583c10bd8ea001145e2b63eae4f4b9af34
LorenzoCiarpaglini <98763178+LorenzoCiarpaglini@users.noreply.github.com>
2022-11-29 16:19:19 +0100

    modified franco

--- franco/app.js
+++ franco/app.js
@@ -5,7 +5,7 @@ const authRouter = require('./routes/authRouter')
 const app = express();
 app.use(express.json());
 
-app.use("auth", authRouter);
+app.use("/auth", authRouter);
 
 let server = app.listen(process.env.PORT || 5000, () => {
     console.log("Server started on port: ", process.env.PORT || 5000);
--- franco/routes/authRouter.js
+++ franco/routes/authRouter.js
@@ -5,20 +5,6 @@ const router = express.Router();
 
 
 router.put("/signUp", authController.signUp)
-// router.put("/signUp", authController.handleResponse)
-
-router.post("/login", authController.login)
-// router.post("/login", authController.handleResponse)
-
-router.patch("/updatePassword", authController.updatePassword)
-
-router.delete("/deleteUser", authController.deleteUser)
-// router.patch("/updatePassword", authController.handleResponse)
-
-// router.get("/getUser", authController.signUp)
-// router.get("/getUser", authController.handleResponse)
-
-router.use(authController.handleResponse)
 
 
 module.exports = router;
